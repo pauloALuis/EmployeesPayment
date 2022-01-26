b8bbce904e8d6ef251c52a1ccdbefc775d74e347
pauloALuis <50427097+pauloALuis@users.noreply.github.com>
2022-01-26 12:01:43 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeesPayment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
